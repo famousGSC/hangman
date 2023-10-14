1a80c8eb9fe654872659ff930944eec673c0eb33
Luke Hand <handl029@gmail.com>
2023-10-14 18:50:22 +0100

    Adds methods to class

--- milestone_4.py
+++ milestone_4.py
@@ -1,5 +1,5 @@
 import random
-guesses=['k','i','p','n', 'r']
+#guesses=['k','i','p','n', 'r']
 
 class Hangman:
     def __init__(self, word_list, num_lives=5):
@@ -14,6 +14,28 @@ class Hangman:
         self.num_letters=len(set(self.hidden))-1 # -1 is so you don't count the '_' character
 
         self.list_of_guesses=[]
+
+    def ask_for_input():
+
+        while True:
+            try:
+                guess = input("Please enter a single letter and press enter: ")
+                if len(guess) != 1:
+                    raise ValueError("Too long")
+                elif guess.isalpha() == False:
+                    raise ValueError("Not alphabetical")
+                else:
+                    return guess
+                break
+            except ValueError:
+                print("Invalid guess. Please, enter a single alphabetical character")
+                continue
+
+    def check_guess(guess):
+        if guess in word:
+            print(f"Good guess, {guess} is in the word!")
+        else:
+            print(f"Bad luck, {guess} is not in the word. Try again.")
 
 
 my_hangman=Hangman(["Banana", "Apple", "Kiwi","Strawberry","Blueberry"])
